95ded605effbecf1a053e62b9e028d1e1a611636
kellolo <87kell@gmail.com>
2021-02-22 20:04:47 +0200

    4

--- ForStudents/Viktor_Katurkin_less-3/src/components/Message/Message.jsx
+++ ForStudents/Viktor_Katurkin_less-3/src/components/Message/Message.jsx
@@ -6,12 +6,12 @@ import './style.scss';
 export default function Message(props){
     
     return(
-        <div className="messege-container">
-        <div className="message-box">
-        <span className="message">
-            {props.el.text}
-        </span>
-        </div>  
+        <div className="message-container">
+            <div className="message-box">
+                <span className="message">
+                    {props.el.text}
+                </span>
+            </div>  
         </div>
         
     )
