504aaef85b1b755ae84de61b78488de781dd13a7
Igor <kpe4ets@gmail.com>
2021-02-26 22:09:17 +0300

    hw4 header

--- ForStudents/ipetrov/Project/src/components/Header/Header.jsx
+++ ForStudents/ipetrov/Project/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@
 
         export default props => {
             return  <div className="header">
-                       Welcome to messenger pre-pre-alpha -0.0.1
+                       Пообщайтесь с {props.name}
             </div>;
         };
     
--- ForStudents/ipetrov/Project/src/router/index.jsx
+++ ForStudents/ipetrov/Project/src/router/index.jsx
@@ -7,7 +7,7 @@ export default class Router extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component = { Home } />
+        <Route exact path="/" render = { () => <Home name="Василий Петрович шиномонтаж" /> } />
         <Route exact path="/chat/1" render = { () => <Home name="Василий Петрович шиномонтаж" /> } />
         <Route exact path="/chat/2" render = { () => <Home name="Эдуард Васильевич прачечная" /> } />
         <Route exact path="/chat/3" render = { () => <Home name="Максим Евгеньевич парикмахерская" /> } />
