ef080193e6528ffd32ffdb158a12cf0982cb6275
kellolo <87kell@gmail.com>
2021-03-01 20:07:35 +0200

    6

---
+++ Project/6/src/Core/Store/actions/messages.js
@@ -0,0 +1,15 @@
+// export const FILTER_MSG = '@@messages/FILTER';
+
+export const loadMessages = () => ({
+  type: 'LOAD_MSG',
+});
+
+export const sendMessage = (name, text) => ({
+  type: 'SEND_MSG',
+  payload: { name, text, date: new Date() }
+});
+
+// export const filterMessages = (searchWord) => ({
+//   type: 'FILTER_MSG',
+//   searchWord
+// });
