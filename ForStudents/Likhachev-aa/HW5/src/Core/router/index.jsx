dfda08091acdae023cff1b21cb422f7aef68d12e
Andrey <likhachev.aa74@gmail.ru>
2021-03-04 22:21:12 +0500

    update

--- ForStudents/Likhachev-aa/HW5/src/Core/router/index.jsx
+++ ForStudents/Likhachev-aa/HW5/src/Core/router/index.jsx
@@ -7,11 +7,11 @@ export default class Router extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component = { Home } />
-        <Route exact path="/chat/1" render = { () => <Home name="Вася" /> } />
-        <Route exact path="/chat/2" render = { () => <Home name="Клава" /> } />
-        <Route exact path="/chat/3" render = { () => <Home name="Дуня" /> } />
-        <Route exact path="/chat/4" render = { () => <Home name="Пердуня" /> } />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/chat/1" render={() => <Home name="Andrew" />} />
+        <Route exact path="/chat/2" render={() => <Home name="Bart" />} />
+        <Route exact path="/chat/3" render={() => <Home name="Tom" />} />
+        <Route exact path="/chat/4" render={() => <Home name="John" />} />
         {/* <Route exact path="/not-home" render = { () => <div>This is not your Home. Get out!</div> } /> */}
       </Switch>
     );
